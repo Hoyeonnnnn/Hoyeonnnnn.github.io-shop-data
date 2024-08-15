aaf39869ef1440c742d4afc58fdc602d1e2666cd
Hoyeonnnnn <116044591+Hoyeonnnnn@users.noreply.github.com>
2024-08-15 17:13:16 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,140 @@
+import adidas_img_1 from './image/adidas_img_1.jpg';
+import asics_img_1 from './image/asics_img_1.jpg'; 
+import bape_img_1 from './image/bape_img_1.jpg';
+import carhatt_img_1 from './image/carhatt_img_1.jpg';
+import chrome_img_1 from './image/chrome_img_1.jpg';
+import iab_img_1 from './image/iab_img_1.jpg';
+import jordan_img_1 from './image/jordan_img_1.jpg'; 
+import martinkim_img_1 from './image/martinkim_img_1.jpg'; 
+import newbalance_img_1 from './image/newbalance_img_1.jpg'; 
+import nike_img_1 from './image/nike_img_1.jpg';
+import palace_img_1 from './image/palace_img_1.jpg';
+import salomon_img_1 from './image/salomon_img_1.jpg';
+import sansangear_img_1 from './image/sansangear_img_1.jpg';
+import stussy_img_1 from './image/stussy_img_1.jpg';
+import supreme_img_1 from './image/supreme_img_1.jpg';
+
+let data = [
+    {
+        id: 0,
+        title: "나이키",
+        contents: "스포츠웨어 및 운동화의 선두 주자, 나이키의 혁신적인 제품들",
+        price: "150,000원",
+        item: "나이키 에어맥스 270",
+        img: nike_img_1
+    },
+    {
+        id: 1,
+        title: "아디다스",
+        contents: "독일의 스포츠웨어 브랜드로, 편안하고 스타일리시한 제품들",
+        price: "130,000원",
+        item: "아디다스 울트라부스트",
+        img: adidas_img_1
+    },
+    {
+        id: 2,
+        title: "스투시",
+        contents: "스트리트웨어의 대명사, 독특한 디자인의 의류와 액세서리",
+        price: "100,000원",
+        item: "스투시 후드티",
+        img: stussy_img_1
+    },
+    {
+        id: 3,
+        title: "크롬하츠",
+        contents: "고급스러운 액세서리와 의류로 유명한 미국 브랜드",
+        price: "800,000원",
+        item: "크롬하츠 목걸이",
+        img: chrome_img_1
+    },
+    {
+        id: 4,
+        title: "아이앱",
+        contents: "트렌디한 패션 아이템을 제공하는 인기 브랜드",
+        price: "90,000원",
+        item: "아이앱 티셔츠",
+        img: iab_img_1
+    },
+    {
+        id: 5,
+        title: "베이프",
+        contents: "일본의 스트리트웨어 브랜드, 개성 있는 디자인의 의류와 신발",
+        price: "250,000원",
+        item: "베이프 스니커즈",
+        img: bape_img_1
+    },
+    {
+        id: 6,
+        title: "슈프림",
+        contents: "뉴욕 기반의 스트리트웨어 브랜드, 한정판 제품들로 유명",
+        price: "300,000원",
+        item: "슈프림 박스 로고 티셔츠",
+        img: supreme_img_1
+    },
+    {
+        id: 7,
+        title: "팔라스",
+        contents: "영국의 스트리트웨어 브랜드, 독특한 로고와 디자인",
+        price: "120,000원",
+        item: "팔라스 후드티",
+        img: palace_img_1
+    },
+    {
+        id: 8,
+        title: "아식스",
+        contents: "편안한 운동화로 유명한 일본 브랜드",
+        price: "110,000원",
+        item: "아식스 젤카야노",
+        img: asics_img_1
+    },
+    {
+        id: 9,
+        title: "칼하트",
+        contents: "견고하고 내구성 있는 작업복 및 스트리트웨어",
+        price: "80,000원",
+        item: "칼하트 워크팬츠",
+        img: carhatt_img_1
+    },
+    {
+        id: 10,
+        title: "살로몬",
+        contents: "등산 및 스포츠용 신발과 의류의 전문 브랜드",
+        price: "200,000원",
+        item: "살로몬 스피드크로스",
+        img: salomon_img_1
+    },
+    {
+        id: 11,
+        title: "조던",
+        contents: "농구화의 대명사, 나이키의 하위 브랜드로 유명",
+        price: "180,000원",
+        item: "조던 1 레트로",
+        img: jordan_img_1
+    },
+    {
+        id: 12,
+        title: "뉴발란스",
+        contents: "편안한 착화감으로 유명한 스포츠화 브랜드",
+        price: "150,000원",
+        item: "뉴발란스 990v5",
+        img: newbalance_img_1
+    },
+    {
+        id: 13,
+        title: "마땡킴",
+        contents: "프리미엄 패션 브랜드, 고급스러운 디자인의 의류",
+        price: "500,000원",
+        item: "마땡킴 드레스",
+        img: martinkim_img_1
+    },
+    {
+        id: 14,
+        title: "산산기어",
+        contents: "산악용품 전문 브랜드, 견고하고 실용적인 제품",
+        price: "300,000원",
+        item: "산산기어 자켓",
+        img: sansangear_img_1
+    },
+]
+
+export default data;
